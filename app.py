0b8db3f165d49ac75478366268d52370c79c99b3
anandr07 <anandraj.1rn17ee005@gmail.com>
2023-12-24 18:15:45 -0500

    Added app.py file

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, jsonify, request
+import numpy as np
+from sklearn.externals import joblib
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.externals import joblib
+from bs4 import BeautifulSoup
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+# https://www.tutorialspoint.com/flask
+import flask
+app = Flask(__name__)
+
+
+###################################################
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+
+stopwords= set(['br', 'the', 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
+            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
+            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
+            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
+            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
+            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
+            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
+            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
+            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
+            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
+            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
+            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
+            'won', "won't", 'wouldn', "wouldn't"])
+
+
+def clean_text(sentence):
+    sentence = re.sub(r"http\S+", "", sentence)
+    sentence = BeautifulSoup(sentence, 'lxml').get_text()
+    sentence = decontracted(sentence)
+    sentence = re.sub("\S*\d\S*", "", sentence).strip()
+    sentence = re.sub('[^A-Za-z]+', ' ', sentence)
+    # https://gist.github.com/sebleier/554280
+    sentence = ' '.join(e.lower() for e in sentence.split() if e.lower() not in stopwords)
+    return sentence.strip()
+###################################################
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    clf = joblib.load('model.pkl')
+    count_vect = joblib.load('count_vect.pkl')
+    to_predict_list = request.form.to_dict()
+    review_text = clean_text(to_predict_list['review_text'])
+    pred = clf.predict(count_vect.transform([review_text]))
+    if pred[0]:
+        prediction = "Positive"
+    else:
+        prediction = "Negative"
+
+    return jsonify({'prediction': prediction})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
